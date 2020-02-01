d5f15131226feb045d52ef90b75eadcffff3f1ab
Tyler Kitchens <tylerkitchens23@gmail.com>
2020-02-01 12:58:51 -0500

    Finished CD and modified ls code

--- DCS-p1/src/ClientHandler.java
+++ DCS-p1/src/ClientHandler.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class ClientHandler implements Runnable {
 	private Socket client;
@@ -13,18 +14,26 @@ public class ClientHandler implements Runnable {
 
 	private static String currDirectory;
 	private static String lastDirectory = null;
+	private String homeDir;
 
 	public ClientHandler(Socket clientSocket) throws IOException {
 		this.client = clientSocket;
 		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		out = new PrintWriter(client.getOutputStream(), true);
 
-		//changes format of directory depending on if server is running windows or linux
-		if(wordParser(System.getProperty("os.name").toCharArray(), 1).contentEquals("Windows")) {
-			currDirectory = System.getProperty("user.dir") + "\\DCS-p1\\root\\";
-		} else{
-			currDirectory = System.getProperty("user.dir") + "/DCS-p1/root/";
-		}
+		/*//changes format of directory depending on if server is running windows or linux, on a side note why do whe have this
+			if(wordParser(System.getProperty("os.name").toCharArray(), 1).contentEquals("Windows")) {
+				currDirectory = System.getProperty("user.dir") ;
+			} else{
+				currDirectory = System.getProperty("user.dir");
+			}
+		 */
+
+		//Should be as simple as this
+		currDirectory = System.getProperty("user.dir");
+
+		//sets the home directory incase you do not enter an argument for CD
+		homeDir = System.getProperty("user.dir");
 	}
 
 	@Override
@@ -41,9 +50,10 @@ public class ClientHandler implements Runnable {
 					run_pwd();
 					break;
 				case "ls":
-					run_ls();
+					run_ls(true);
 					break;
 				case "cd":
+					run_cd(request);
 					break;
 				case "get":
 					break;
@@ -77,17 +87,26 @@ public class ClientHandler implements Runnable {
 		} // finally
 	}
 
-	public void run_ls() {
-		String s = null;
-		File dir = new File(System.getProperty("user.dir")+"\\DCS-p1\\root");
+	public String run_ls(boolean print) {
+		String s = "";
+		//Why do we have to make that folder. This allows it to work no matter where the program is 
+		File dir = new File(currDirectory);
 		File[] childs = dir.listFiles();
+
 		try{
 			for(File child: childs){
-				s = child.getName() + ' ';
-			}//for
-			out.println(s);
+
+				//Changed this to += so now it works, no hidden files in ls
+				if(child.getName().charAt(0) != '.')
+					s += child.getName() + ' ';
+			}
+			//added this for my benefit of your code
+			if(print)
+				out.println(s);
+			return s;
 		} catch (NullPointerException e) {
-			out.println("\n");
+			out.println("You Fucked up\n");
+			return s;
 		}
 	}//run_ls
 
@@ -110,7 +129,30 @@ public class ClientHandler implements Runnable {
 	}
 
 	public void run_cd(String directory) {
+		//have to add a space to the end to get it to split easier
+		directory += " ";
+		String[] commandWords = directory.split(" ");
 
+		//User wants to go back to home
+		if(commandWords.length == 1){
+			currDirectory = homeDir;
+			//need these outs to make new prompt
+			out.println();
+		}else if(commandWords[1].equals("..")){
+			parentDir();
+			out.println();
+		}else{
+			if(isValidPath(commandWords[1])){
+				if(commandWords[1].charAt(0) == '\\'){
+					currDirectory = commandWords[1];
+				}else{
+					currDirectory += commandWords[1];
+				}
+				out.println();
+			}else{
+				out.println("cd: " + commandWords[1] + " No such file or directory");
+			}
+		}
 	}
 
 	public void run_mkdir(String directory) {
@@ -138,5 +180,27 @@ public class ClientHandler implements Runnable {
 		}
 
 		return s;
+	}
+
+	public void parentDir(){
+		String[] dirArr = currDirectory.split("\\\\");
+		String temp = "";
+		//Loop till last item and do not add it
+		for(int i = 0; i< dirArr.length -1; i++){
+			temp+= dirArr[i] + "\\";
+		}
+
+		currDirectory = temp;
+	}
+
+	public boolean isValidPath(String path){
+		//its an absolute path, check validity
+		if(path.charAt(0) == '\\'){
+			return new File(path).exists();
+		}else{
+			//looking for folder inside this folder
+			return run_ls(false).contains(path);
+		}
+
 	}
 }
