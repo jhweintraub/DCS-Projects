df853d3fc0f5eff28a01bd041c28177021578c36
Josh Weintraub <jhweintraub@gmail.com>
2020-02-25 11:44:36 -0500

    Project 2 - First Commit. Job Class Added. Added Extra Sockets to the Server and Client but multithreaded client TODO

---
+++ DCS-p2-multithreading/src/TerminateHandler.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TerminateHandler implements Runnable {
+	private Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+	private DataOutputStream dOut;
+	private Long threadId;
+	
+	public TerminateHandler(Socket clientSocket, Long threadID) throws IOException {
+		this.client = clientSocket;
+		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		out = new PrintWriter(client.getOutputStream(), true);
+		this.dOut = new DataOutputStream(client.getOutputStream());
+		this.threadId = threadID;
+
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Job term_job = Server.clients.get(Server.getIndexofThreat(threadId));
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
