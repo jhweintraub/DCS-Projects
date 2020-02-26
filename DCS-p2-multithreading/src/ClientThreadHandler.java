d72c25c1aa86d2093b9e630e3a4bbe183feda6d0
Josh Weintraub <jhweintraub@gmail.com>
2020-02-26 14:54:50 -0500

    TODO List, new threading class for client, misc.

---
+++ DCS-p2-multithreading/src/ClientThreadHandler.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientThreadHandler implements Runnable {
+	
+	private Socket execSocket;
+	private Socket termSocket;
+	private BufferedReader exec_input;
+	private BufferedReader term_input;
+	private PrintWriter out;
+	private PrintWriter out_term;
+	private String command;
+
+
+	BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+	// This is what writes to the socket - you use out.print() and whats inside the
+	// method is what gets written to the socket
+	
+
+	
+
+	/*This Class is where the new thread gets executed when the user appends their command with &. It's basically
+	the same as the Client.java but it's in a thread*/
+	
+	public ClientThreadHandler(Socket execSocket, Socket termSocket, String command) throws IOException {
+		this.execSocket = execSocket;
+		this.termSocket = termSocket;
+		
+		
+		this.exec_input = new BufferedReader(new InputStreamReader(execSocket.getInputStream()));
+		this.term_input = new BufferedReader(new InputStreamReader(termSocket.getInputStream()));
+		
+		this.out = new PrintWriter(execSocket.getOutputStream(), true);
+		this.out_term = new PrintWriter(termSocket.getOutputStream(), true);
+		
+		this.command = command;
+	}
+	
+	@Override
+	public void run() {
+		//TODO - Copy the Code from the Client.Java Class Essentially
+	}
+
+}
---
+++ DCS-p2-multithreading/src/TODO.md
@@ -0,0 +1,4 @@
+1. Implement delete thread
+2. implement semaphore lock/release mech
+3. implement ClientThreadHandler for Client side.
+4. Finish Implementing Helper Methods for ClientHandler
